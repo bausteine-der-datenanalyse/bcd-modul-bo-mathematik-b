a7af524e310ada764b182fe64d5f22ae201b1ed2
FrancaHollmann <franca.hollmann@hs-bochum.de>
2025-10-08 10:40:56 +0200

    Pfad korrigiert

--- administration/01-bootstrap.bat
+++ administration/01-bootstrap.bat
@@ -1,5 +1,6 @@
 set R="%USERPROFILE%\AppData\Local\Programs\R\R-4.5.1\bin\R"
 
+cd ..
 %R% -e "install.packages(\"remotes\", repos = \"https://cran.uni-muenster.de\"); remotes::install_deps(upgrade = \"always\")" 
 
 pause
