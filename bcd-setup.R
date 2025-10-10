9ac979d1976bcc36ca9fe77f756668eec1626017
Matthias Baitsch <matthias.baitsch@me.com>
2025-10-10 12:30:08 +0200

    WIP

--- bcd-setup.R
+++ bcd-setup.R
@@ -15,14 +15,16 @@ update_geom_defaults("point", list(fill = fill_point_color, color = outline_colo
 update_geom_defaults("boxplot", list(fill = fill_box_color, color = outline_color))
 update_geom_defaults("dotplot", list(fill = fill_box_color, color = outline_color))
 
-knitr::opts_chunk$set(
-    fig.width = 9, fig.asp = 0.618, fig.aligh = "center", out.width = "85%", echo = FALSE
-)
-
 if (knitr::is_latex_output()) {
+    knitr::opts_chunk$set(
+        fig.width = 9, fig.asp = 0.618, fig.aligh = "center", out.width = "85%", echo = FALSE
+    )
     theme_set(theme_bw(base_size = 16))
 }
 
 if (knitr::is_html_output()) {
+    knitr::opts_chunk$set(
+        fig.width = 9, fig.asp = 0.618, fig.aligh = "center", out.width = "85%", echo = FALSE
+    )
     theme_set(theme_gray(base_size = 16))
 }
